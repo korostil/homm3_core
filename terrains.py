74e917afe3e64ae76ac26f6b06304c77fd67c77b
korostil <av.korostil@gmail.com>
2021-01-04 01:08:47 +0300

    Replace terrains and towns with enum

--- terrains.py
+++ terrains.py
@@ -1,27 +1,16 @@
-GRASS = 0
-HIGHLANDS = 1
-DIRT = 2
-LAVA = 3
-SUBTERRANEAN = 4
-ROCK = 5
-ROUGH = 6
-WASTELAND = 7
-SAND = 8
-SNOW = 9
-SWAMP = 10
-WATER = 11
+import enum
 
-TERRAINS = {
-    "Grass": GRASS,
-    "Highlands": HIGHLANDS,
-    "Dirt": DIRT,
-    "Lava": LAVA,
-    "Subterranean": SUBTERRANEAN,
-    "Rock": ROCK,
-    "Rough": ROUGH,
-    "Wasteland": WASTELAND,
-    "Sand": SAND,
-    "Snow": SNOW,
-    "Swamp": SWAMP,
-    "Water": WATER
-}
+
+class Terrains(enum.Enum):
+    GRASS = 0
+    HIGHLANDS = 1
+    DIRT = 2
+    LAVA = 3
+    SUBTERRANEAN = 4
+    ROCK = 5
+    ROUGH = 6
+    WASTELAND = 7
+    SAND = 8
+    SNOW = 9
+    SWAMP = 10
+    WATER = 11
--- towns.py
+++ towns.py
@@ -1,25 +1,15 @@
-CASTLE = 0
-RAMPART = 1
-TOWER = 2
-INFERNO = 3
-NECROPOLIS = 4
-DUNGEON = 5
-STRONGHOLD = 6
-FORTRESS = 7
-CONFLUX = 8
-COVE = 9
-NEUTRAL = 10
+import enum
 
-TOWNS = {
-    "Castle": CASTLE,
-    "Rampart": RAMPART,
-    "Tower": TOWER,
-    "Inferno": INFERNO,
-    "Necropolis": NECROPOLIS,
-    "Dungeon": DUNGEON,
-    "Stronghold": STRONGHOLD,
-    "Fortress": FORTRESS,
-    "Conflux": CONFLUX,
-    "Cove": COVE,
-    "Neutral": NEUTRAL,
-}
+
+class Towns(enum.Enum):
+    CASTLE = 0
+    RAMPART = 1
+    TOWER = 2
+    INFERNO = 3
+    NECROPOLIS = 4
+    DUNGEON = 5
+    STRONGHOLD = 6
+    FORTRESS = 7
+    CONFLUX = 8
+    COVE = 9
+    NEUTRAL = 10
